801a11803a2ffbc7cb4d0a7f3c8cff7ef1115afa
Saima Safdar <safdarsaima@ymail.com>
2022-05-09 12:07:58 +0800

    Electrode locations on an undeformed brain geometry. Projecting the electrode locations on undeformed brain surface using the electrode locations identified from intra-operative CT

---
+++ FiducialToModelDistance/FiducialToModelDistance.py
@@ -0,0 +1,319 @@
+import os
+import unittest
+import logging
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+
+#
+# FiducialToModelDistance
+#
+
+class FiducialToModelDistance(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "FiducialToModelDistance"  # TODO: make this more human readable by adding spaces
+    self.parent.categories = ["Image as a Model"]  # TODO: set categories (folders where the module shows up in the module selector)
+    self.parent.dependencies = []  # TODO: add here list of module names that this module requires
+    self.parent.contributors = ["Saima Safdar"]  # TODO: replace with "Firstname Lastname (Organization)"
+    self.parent.helpText = """
+This is an example of scripted loadable module bundled in an extension.
+It performs a simple thresholding on the input volume and optionally captures a screenshot.
+"""  # TODO: update with short description of the module
+    self.parent.helpText += self.getDefaultModuleDocumentationLink()  # TODO: verify that the default URL is correct or change it to the actual documentation
+    self.parent.acknowledgementText = """
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+"""  # TODO: replace with organization, grant and thanks.
+
+#
+# FiducialToModelDistanceWidget
+#
+
+class FiducialToModelDistanceWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent=None):
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+    VTKObservationMixin.__init__(self)  # needed for parameter node observation
+    self.logic = None
+    self._parameterNode = None
+
+  def setup(self):
+    """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # Load widget from .ui file (created by Qt Designer)
+    uiWidget = slicer.util.loadUI(self.resourcePath('UI/FiducialToModelDistance.ui'))
+    self.layout.addWidget(uiWidget)
+    self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+    # Set scene in MRML widgets. Make sure that in Qt designer
+    # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+    # "setMRMLScene(vtkMRMLScene*)" slot.
+    uiWidget.setMRMLScene(slicer.mrmlScene)
+
+    # Create a new parameterNode (it stores user's node and parameter values choices in the scene)
+    self.logic = FiducialToModelDistanceLogic()
+    self.ui.parameterNodeSelector.addAttribute("vtkMRMLScriptedModuleNode", "ModuleName", self.moduleName)
+    self.setParameterNode(self.logic.getParameterNode())
+
+    # Connections
+    self.ui.parameterNodeSelector.connect('currentNodeChanged(vtkMRMLNode*)', self.setParameterNode)
+
+    self.ui.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
+    self.ui.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.updateParameterNodeFromGUI)
+    self.ui.applyButton.connect('clicked(bool)', self.onApplyButton)
+
+    # Initial GUI update
+    self.updateGUIFromParameterNode()
+
+  def cleanup(self):
+    """
+    Called when the application closes and the module widget is destroyed.
+    """
+    self.removeObservers()
+
+  def setParameterNode(self, inputParameterNode):
+    """
+    Adds observers to the selected parameter node. Observation is needed because when the
+    parameter node is changed then the GUI must be updated immediately.
+    """
+
+    if inputParameterNode:
+      self.logic.setDefaultParameters(inputParameterNode)
+    wasBlocked = self.ui.parameterNodeSelector.blockSignals(True)
+    self.ui.parameterNodeSelector.setCurrentNode(inputParameterNode)
+    self.ui.parameterNodeSelector.blockSignals(wasBlocked)
+    if inputParameterNode == self._parameterNode:
+      return
+    if self._parameterNode is not None:
+      self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+    if inputParameterNode is not None:
+      self.addObserver(inputParameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+    self._parameterNode = inputParameterNode
+    self.updateGUIFromParameterNode()
+
+  def updateGUIFromParameterNode(self, caller=None, event=None):
+    """
+    This method is called whenever parameter node is changed.
+    The module GUI is updated to show the current state of the parameter node.
+    """
+
+    # Disable all sections if no parameter node is selected
+    self.ui.basicCollapsibleButton.enabled = self._parameterNode is not None
+    
+    if self._parameterNode is None:
+      return
+
+    # Update each widget from parameter node
+
+    wasBlocked = self.ui.inputSelector.blockSignals(True)
+    self.ui.inputSelector.setCurrentNode(self._parameterNode.GetNodeReference("InputVolume"))
+    self.ui.inputSelector.blockSignals(wasBlocked)
+
+    wasBlocked = self.ui.outputSelector.blockSignals(True)
+    self.ui.outputSelector.setCurrentNode(self._parameterNode.GetNodeReference("OutputVolume"))
+    self.ui.outputSelector.blockSignals(wasBlocked)
+
+    
+
+    # Update buttons states and tooltips
+    if self._parameterNode.GetNodeReference("InputVolume") and self._parameterNode.GetNodeReference("OutputVolume"):
+      self.ui.applyButton.toolTip = "Compute output volume"
+      self.ui.applyButton.enabled = True
+    else:
+      self.ui.applyButton.toolTip = "Select input and output volume nodes"
+      print("here")
+      self.ui.applyButton.enabled = False
+
+  def updateParameterNodeFromGUI(self, caller=None, event=None):
+    """
+    This method is called when the user makes changes in the GUI.
+    The changes are saved into the parameter node (so that they are preserved when the scene is saved and loaded).
+    """
+
+    if self._parameterNode is None:
+      return
+
+    self._parameterNode.SetNodeReferenceID("InputVolume", self.ui.inputSelector.currentNodeID)
+    self._parameterNode.SetNodeReferenceID("OutputVolume", self.ui.outputSelector.currentNodeID)
+    
+
+  def onApplyButton(self):
+    """
+    Run processing when user clicks "Apply" button.
+    """
+    try:
+      self.logic.run(self.ui.inputSelector.currentNode(), self.ui.outputSelector.currentNode())
+    except Exception as e:
+      slicer.util.errorDisplay("Failed to compute results: "+str(e))
+      import traceback
+      traceback.print_exc()
+
+
+#
+# FiducialToModelDistanceLogic
+#
+
+class FiducialToModelDistanceLogic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setDefaultParameters(self, parameterNode):
+    if not parameterNode.GetParameter("Threshold"):
+      parameterNode.SetParameter("Threshold", "50.0")
+    if not parameterNode.GetParameter("Invert"):
+      parameterNode.SetParameter("Invert", "false")
+
+  def run(self, inputVolume, outputVolume):
+    """
+    Run the processing algorithm.
+    Can be used without GUI widget.
+    """
+
+    if not inputVolume or not outputVolume:
+      raise ValueError("Input or output volume is invalid")
+
+    logging.info('Processing started')
+
+    # Compute the thresholded output volume using the Threshold Scalar Volume CLI module
+    markupsNode = slicer.util.getNode(inputVolume.GetID())
+    modelNode = slicer.util.getNode(outputVolume.GetID())
+    
+    # Transform model polydata to world coordinate system
+    if modelNode.GetParentTransformNode():
+      transformModelToWorld = vtk.vtkGeneralTransform()
+      slicer.vtkMRMLTransformNode.GetTransformBetweenNodes(modelNode.GetParentTransformNode(), None, transformModelToWorld)
+      polyTransformToWorld = vtk.vtkTransformPolyDataFilter()
+      polyTransformToWorld.SetTransform(transformModelToWorld)
+      polyTransformToWorld.SetInputData(modelNode.GetPolyData())
+      polyTransformToWorld.Update()
+      surface_World = polyTransformToWorld.GetOutput()
+    else:
+      surface_World = modelNode.GetPolyData()
+    
+    # Create arrays to store results
+    indexCol = vtk.vtkIntArray()
+    indexCol.SetName("Index")
+    labelCol = vtk.vtkStringArray()
+    labelCol.SetName("Name")
+    distanceCol = vtk.vtkDoubleArray()
+    distanceCol.SetName("Distance")
+    
+    markupsNode2 = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsFiducialNode")
+    markupsNode2.CreateDefaultDisplayNodes()
+    
+    distanceFilter = vtk.vtkImplicitPolyDataDistance()
+    distanceFilter.SetInput(surface_World);
+    nOfFiduciallPoints = markupsNode.GetNumberOfFiducials()
+    for i in range(0, nOfFiduciallPoints):
+      point_World = [0,0,0]
+      markupsNode.GetNthControlPointPositionWorld(i, point_World)
+      closestPointOnSurface_World = [0,0,0]
+      closestPointDistance = distanceFilter.EvaluateFunctionAndGetClosestPoint(point_World, closestPointOnSurface_World)
+      markupsNode2.AddFiducial(closestPointOnSurface_World[0],closestPointOnSurface_World[1],closestPointOnSurface_World[2])
+      indexCol.InsertNextValue(i)
+      labelCol.InsertNextValue(markupsNode.GetNthControlPointLabel(i))
+      distanceCol.InsertNextValue(closestPointDistance)
+    
+    # Create a table from result arrays
+    resultTableNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTableNode", "Points from surface distance")
+    resultTableNode.AddColumn(indexCol)
+    resultTableNode.AddColumn(labelCol)
+    resultTableNode.AddColumn(distanceCol)
+    
+    # Show table in view layout
+    slicer.app.layoutManager().setLayout(slicer.vtkMRMLLayoutNode.SlicerLayoutFourUpTableView)
+    slicer.app.applicationLogic().GetSelectionNode().SetReferenceActiveTableID(resultTableNode.GetID())
+    slicer.app.applicationLogic().PropagateTableSelection()
+    
+    
+    
+
+    logging.info('Processing completed')
+
+#
+# FiducialToModelDistanceTest
+#
+
+class FiducialToModelDistanceTest(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_FiducialToModelDistance1()
+
+  def test_FiducialToModelDistance1(self):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests should exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+
+    # Get/create input data
+
+    import SampleData
+    inputVolume = SampleData.downloadFromURL(
+      nodeNames='MRHead',
+      fileNames='MR-Head.nrrd',
+      uris='https://github.com/Slicer/SlicerTestingData/releases/download/MD5/39b01631b7b38232a220007230624c8e',
+      checksums='MD5:39b01631b7b38232a220007230624c8e')[0]
+    self.delayDisplay('Finished with download and loading')
+
+    inputScalarRange = inputVolume.GetImageData().GetScalarRange()
+    self.assertEqual(inputScalarRange[0], 0)
+    self.assertEqual(inputScalarRange[1], 279)
+
+    outputVolume = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
+    threshold = 50
+
+    # Test the module logic
+
+    logic = FiducialToModelDistanceLogic()
+
+    # Test algorithm with non-inverted threshold
+    logic.run(inputVolume, outputVolume, threshold, False)
+    outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+    self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+    self.assertEqual(outputScalarRange[1], threshold)
+
+    # Test algorithm with inverted threshold
+    logic.run(inputVolume, outputVolume, threshold, True)
+    outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+    self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+    self.assertEqual(outputScalarRange[1], inputScalarRange[1])
+
+    self.delayDisplay('Test passed')
