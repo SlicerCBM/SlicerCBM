e45dfd25a0d8566c9d848ae7dc136a20e5382690
Saima Safdar <safdarsaima@ymail.com>
2023-05-04 11:21:54 +0800

    library python-math added

--- CranCreator/CranCreator.py
+++ CranCreator/CranCreator.py
@@ -17,7 +17,7 @@ class CranCreator(ScriptedLoadableModule):
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
     self.parent.title = "CranCreator"  # TODO: make this more human readable by adding spaces
-    self.parent.categories = ["CBM.Biomechanical"]  # TODO: set categories (folders where the module shows up in the module selector)
+    self.parent.categories = ["CBM.Biomechanical.Craniotomy"]  # TODO: set categories (folders where the module shows up in the module selector)
     self.parent.dependencies = []  # TODO: add here list of module names that this module requires
     self.parent.contributors = ["Saima Safdar"]  # TODO: replace with "Firstname Lastname (Organization)"
     # TODO: update with short description of the module and a link to online module documentation
